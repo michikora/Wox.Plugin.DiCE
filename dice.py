47044c1327d9ddbf4750f9f8b5600aa149e66b9c
michikora <i@michikora.com>
2016-12-29 05:14:11 +0800

    基本功能完成
    暂时没有限制骰子的粒数和面数，所以可以投类似1234d56789这类的不实际的骰子，请谨慎使用。
    之后会添加限制并加上对投骰结果的加减算功能。

---
+++ dice.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import re
+import random
+import os
+from wox import Wox, WoxAPI
+
+class OmnipotentScepter(Wox):
+
+    def query(self, query):
+        result = []
+        if not query:
+            result.append({
+                "Title": "Let's roll some dice!",
+                "SubTitle":"You can use '1d20','1D20','1 20','1,20','1.20' to rolling dice.",
+                "IcoPath":"icon/dice.png",
+            })
+            return result
+
+        diceParam = list(filter(lambda x: len(x) > 0, re.split('\s|d|D|,|\.', query)))
+
+        if len(diceParam) != 2:
+            result.append({
+                "Title": "Missing parameters",
+                "SubTitle":"Please make sure you enter two values,and use correct symbol.",
+                "IcoPath":"icon/dice.png",
+            })
+            return result
+
+        try:
+            freq = int(diceParam[0])
+            surf = int(diceParam[1])
+        except ValueError:
+            result.append({
+                "Title": "Value invalid",
+                "SubTitle":"Please use Numbers.",
+                "IcoPath":"icon/dice.png",
+            })
+        else:
+            diceResult = []
+            for num in range(0,freq):
+                rollDice = random.randint(1,surf)
+                diceResult.append(rollDice)
+            diceResultTotal = sum(diceResult)
+
+            diceResult = str(diceResult)
+            diceResultTotal = str(diceResultTotal)
+            result.append({
+                "Title": "Result: " + diceResultTotal,
+                "SubTitle":"Detail: " + diceResult,
+                "IcoPath":"icon/dice.png",
+            })
+            return result
+
+
+
+if __name__ == "__main__":
+    OmnipotentScepter()
